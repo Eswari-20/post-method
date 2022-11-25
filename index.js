6e03f8499a89d4b3623a89abb52829b3e5c4bf8c
Eswari-20 <eswari.thopuri2003@gmail.com>
2022-11-25 16:52:49 +0530

    postmethod

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.get("/",(req,res)=>{
 
 app.post("/newData",(req,res)=>{
 	//const name=req.body.name,age=req.body.age
-	const {name,password}=req.body;
-	console.log(name,password)
+	const {name,password,gender}=req.body;
+	console.log(name,password,gender)
 	res.send("added Successfully")
 })
 
